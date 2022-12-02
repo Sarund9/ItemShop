0287727bc24b5cd93aff5baee8da7bb2d92c3a95
Sarund9 <sebi.obrutsky@gmail.com>
2022-12-02 00:05:15 -0300

    Equip Objects

---
+++ Assets/_ItemShop/Runtime/UI/PlayerClicker.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ItemShop
+{
+    public class PlayerClicker : MonoBehaviour, IContainer
+    {
+
+        [SerializeField]
+        Vector2 offset;
+
+        [SerializeField]
+        Item[] items = new Item[1];
+
+        Camera cam;
+        Transform player;
+
+        public event Action<int> OnItemChanged;
+
+        public int Width => items.Length;
+
+        public int Height => 1;
+
+        public Item this[int slot] {
+            get => items[slot];
+            set {
+                items[slot] = value;
+                OnItemChanged?.Invoke(slot);
+            }
+        }
+
+        private void Start()
+        {
+            cam = GM.Camera;
+            player = PlayerInventory.Instance.transform;
+            //OnValidate();
+        }
+
+        //private void OnValidate()
+        //{
+        //    items = new Item[slotCount];
+        //}
+
+        private void Update()
+        {
+            var pos = cam.WorldToScreenPoint(player.position);
+
+            transform.position = pos + (Vector3)offset;
+        }
+
+        public void Clicked(int slot)
+        {
+            var d = PlayerInventory.Instance;
+            
+            d.ItemSwapped(this, slot);
+        }
+    }
+}
